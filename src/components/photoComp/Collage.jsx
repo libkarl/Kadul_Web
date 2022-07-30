18e9a501f7eacb3401a63dacd436dd97e191a2ad
libkarl <karel.hb@email.cz>
2022-07-30 21:10:51 +0200

    Add popover MUI

--- src/components/photoComp/Collage.jsx
+++ src/components/photoComp/Collage.jsx
@@ -1,12 +1,30 @@
-import React, { Component} from 'react'
+import * as React from 'react';
 import "./collage.css"
 import Cozy_homes from "../../assets/cozy_homes.jpg"
 import Residence from "../../assets/residence.jpg"
 import Architect from "../../assets/architect.jpg"
 import Arch_tec from "../../assets/arch_tec.jpg"
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-class Collage extends Component {
-    render() {
+
+// Functional component
+export default function Collage() {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+    
         return(
             <>
                 <div class="mx-auto text-center p-6 w-11/12 container">
@@ -21,7 +39,25 @@ class Collage extends Component {
                                     <img src={Cozy_homes} alt="plant" class="h-auto w-full rounded-2xl" />
                                     <div class="p-6">
                                     <p class="text-medium mb-5 text-gray-700">Well, aren't you going up to the lake tonight, you've been planning it for two weeks.</p>
-                                    <button class="w-44 rounded-md py-3  bg-black text-white border-2 hover:bg-white hover:text-black hover:shadow-md duration-75 hover:border-2 border-black">See More</button>
+                                    <Button class="w-44 rounded-md py-3  bg-black text-white border-2 hover:bg-white hover:text-black hover:shadow-md duration-75 hover:border-2 border-black"
+                                    aria-describedby={id}
+                                    variant="contained" 
+                                    onClick={handleClick}
+                                    >See More
+                                    </Button>
+                                    <Popover
+                                        
+                                        id={id}
+                                        open={open}
+                                        anchorEl={anchorEl}
+                                        onClose={handleClose}
+                                        anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                        }}
+                                        >
+                                        <Typography class="italic max-w-prose hover:not-italic text-lg p-6 "sx={{ p: 2 }}>The content of the Popover. The content of the Popover. The content of the Popover.</Typography>
+                                    </Popover>
                                     </div>
                                 </div>
                             </div>
@@ -33,7 +69,25 @@ class Collage extends Component {
                                     <img src={Residence} alt="plant" class="h-auto w-full rounded-2xl" />
                                     <div class="p-6">
                                     <p class="text-medium mb-5 text-gray-700">Well, aren't you going up to the lake tonight, you've been planning it for two weeks.</p>
-                                    <button class="w-44 rounded-md py-3  bg-black text-white border-2 hover:bg-white hover:text-black hover:shadow-md duration-75 hover:border-2 border-black">See More</button>
+                                    <Button class="w-44 rounded-md py-3  bg-black text-white border-2 hover:bg-white hover:text-black hover:shadow-md duration-75 hover:border-2 border-black"
+                                    aria-describedby={id}
+                                    variant="contained" 
+                                    onClick={handleClick}
+                                    >See More
+                                    </Button>
+                                    <Popover
+                                        
+                                        id={id}
+                                        open={open}
+                                        anchorEl={anchorEl}
+                                        onClose={handleClose}
+                                        anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                        }}
+                                        >
+                                        <Typography class="italic max-w-prose hover:not-italic text-lg p-6 "sx={{ p: 2 }}>The content of the Popover. The content of the Popover. The content of the Popover.</Typography>
+                                    </Popover>
                                     </div>
                                 </div>
                             </div>
@@ -45,7 +99,25 @@ class Collage extends Component {
                                     <img src={Architect} alt="plant" class="h-110 w-full rounded-2xl" />
                                     <div class="p-6">
                                     <p class="text-medium mb-5 text-gray-700">Well, aren't you going up to the lake tonight, you've been planning it for two weeks.</p>
-                                    <button class="w-44 rounded-md py-3  bg-black text-white border-2 hover:bg-white hover:text-black hover:shadow-md duration-75 hover:border-2 border-black">See More</button>
+                                    <Button class="w-44 rounded-md py-3  bg-black text-white border-2 hover:bg-white hover:text-black hover:shadow-md duration-75 hover:border-2 border-black"
+                                    aria-describedby={id}
+                                    variant="contained" 
+                                    onClick={handleClick}
+                                    >See More
+                                    </Button>
+                                    <Popover
+                                        
+                                        id={id}
+                                        open={open}
+                                        anchorEl={anchorEl}
+                                        onClose={handleClose}
+                                        anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                        }}
+                                        >
+                                        <Typography class="italic max-w-prose hover:not-italic text-lg p-6 "sx={{ p: 2 }}>The content of the Popover. The content of the Popover. The content of the Popover.</Typography>
+                                    </Popover>
                                     </div>
                                 </div>
                             </div>
@@ -57,7 +129,25 @@ class Collage extends Component {
                                     <img src={Arch_tec} alt="plant" class="h-auto w-full rounded-2xl" />
                                     <div class="p-6">
                                     <p class="text-medium mb-5 text-gray-700">Well, aren't you going up to the lake tonight, you've been planning it for two weeks.</p>
-                                    <button class="w-44 rounded-md py-3  bg-black text-white border-2 hover:bg-white hover:text-black hover:shadow-md duration-75 hover:border-2 border-black">See More</button>
+                                    <Button class="w-44 rounded-md py-3  bg-black text-white border-2 hover:bg-white hover:text-black hover:shadow-md duration-75 hover:border-2 border-black"
+                                    aria-describedby={id}
+                                    variant="contained" 
+                                    onClick={handleClick}
+                                    >See More
+                                    </Button>
+                                    <Popover
+                                        
+                                        id={id}
+                                        open={open}
+                                        anchorEl={anchorEl}
+                                        onClose={handleClose}
+                                        anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                        }}
+                                        >
+                                        <Typography class="italic max-w-prose hover:not-italic text-lg p-6 "sx={{ p: 2 }}>The content of the Popover. The content of the Popover. The content of the Popover.</Typography>
+                                    </Popover>
                                     </div>
                                 </div>
                             </div>
@@ -76,8 +166,7 @@ class Collage extends Component {
         );
             
 
-    }
+    
 }    
 
 
-export default Collage
